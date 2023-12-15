c988bdd4621f96e2a955fd020830a4cc5c93607c
Nikyl S <103478210+NikylS007@users.noreply.github.com>
2023-12-16 02:45:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import random
+
+app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route('/get_random_word', methods=['GET'])
+def get_random_word():
+    words = ['nikyl', 'sandy', 'madhu', 'fida', 'mano', 'sacky', 'narasingan', 'Krishna', 'lathish']
+    random_word = random.choice(words)
+    return jsonify({'word': random_word})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
