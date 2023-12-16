1648d929c98a715751a4a0aaca5c184972b0b75f
Nikyl S <nikylsmart2001@gmail.com>
2023-12-16 15:33:10 +0530

    name change

--- app.py
+++ app.py
@@ -1,34 +1,75 @@
+import os.path
+from flask_cors import CORS
+from flask import Flask, jsonify
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-SPREADSHEET_ID = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
-RANGE_NAME = "Sheet1!A1:E5"  # Replace with your specific range
+app = Flask(__name__)
+cors = CORS(app)
 
-# Load credentials from the downloaded JSON file
-credentials = Credentials.from_authorized_user_file("token.json", SCOPES)(
-    service_account.Credentials.from_service_account_file(
-    "credentials.json",
-    scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"],
-))
 
-# Create a Google Sheets API service
-service = googleapiclient.discovery.build("sheets", "v4", credentials=credentials)
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-# Call the Sheets API
-result = (
-    service.spreadsheets()
-    .values()
-    .get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME)
-    .execute()
-)
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1yTOn046dB0ig97Np8w9ksDEihCTitrgWkUO4_X-3vOw"
+SAMPLE_RANGE_NAME = "Form responses 1!A2:G"
 
-values = result.get("values", [])
 
-if not values:
-    print("No data found.")
-else:
-    for row in values:
-        print(row)
+def cred():
+    """Shows basic usage of the Sheets API.
+  Prints values from a sample spreadsheet.
+  """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values()
+            .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+            .execute()
+        )
+        values = result.get("values", [])
+        if not values:
+            print("No data found.")
+            return
+        return values
+
+    except HttpError as err:
+        print(err)
+
+
+@app.route('/api/latest_episode', methods=['GET'])
+def get_latest_episode():
+    values = cred()
+    return jsonify(values[-1])
+
+
+@app.route('/api/all_episodes', methods=['GET'])
+def get_all_episodes():
+    values = cred()
+    return jsonify(values)
+
